21ef722d87d28b2d3b3481699c58cb2780143d1d
lidia-freitas <lydiaoliveira@gmail.com>
2017-10-07 13:26:44 -0300

    adjustment for best practices

--- counter.js
+++ counter.js
@@ -1,15 +1,18 @@
 //REDUCER
-function counter(currentState, action) {
-    var nextState = {
-        count: currentState.count
+function counter(state, action) {
+    if(typeof state === 'undefined'){
+        return {count : 0}
     }
+    var nextState = {
+        count: state.count
+    };
     switch (action.type) {
         case 'ADD':
-            nextState.count = currentState.count + 1;
+            nextState.count = state.count + 1;
             return nextState;
             break;
         case 'MINUS':
-            nextState.count = currentState.count - 1;
+            nextState.count = state.count - 1;
             return nextState;
             break;
         case 'RESET':
@@ -17,35 +20,35 @@ function counter(currentState, action) {
             return nextState;
             break;
         default:
-        console.log('In Default');
-        return currentState;
+        return state;
     }
 }
 
-var state = { count: 0 };
-var store = Redux.createStore(counter, state);
+//STORE
+var state = { count : 0 }
+var store = Redux.createStore(counter);
 var counterEl = document.getElementById('counter');
 
-function render(){
-    console.log('In Render');
-    console.log(store.getState());
+function render() {
     var state = store.getState();
     counterEl.innerHTML = state.count.toString();
 }
-store.subscribe(render)
+
+render();
+store.subscribe(render);
 
 //ACTIONS
 document.getElementById('add')
     .addEventListener('click', function () {
-            store.dispatch({type: 'ADD'});
+        store.dispatch({type: 'ADD'})
     });
 
 document.getElementById('minus')
     .addEventListener('click', function () {
-            store.dispatch({type: 'MINUS'});
+        store.dispatch({type: 'MINUS'})
     });
 
 document.getElementById('reset')
     .addEventListener('click', function () {
-            store.dispatch({type: 'RESET'});
+        store.dispatch({type: 'RESET'})
     });
